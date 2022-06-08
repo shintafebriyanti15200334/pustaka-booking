8c4602c1fd8c570e98a6097632219a40749b788e
shintafebriyanti15200334 <shintafebriyanti633@gmail.com>
2022-06-08 17:06:52 +0700

    wp2

--- index.php
+++ index.php
@@ -311,5 +311,6 @@ switch (ENVIRONMENT)
  * --------------------------------------------------------------------
  *
  * And away we go...
+ * test
  */
 require_once BASEPATH.'core/CodeIgniter.php';
